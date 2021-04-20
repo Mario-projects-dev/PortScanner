9b8fce749b3bbc3ecdc3ef0e697d2be5a8eb03af
Mario-projects-dev <m.vitek.mv@gmail.com>
2021-04-20 21:14:29 +0200

    initializing code

--- main.cpp
+++ main.cpp
@@ -23,72 +23,13 @@ int main(int argc, char **argv)
 
     {
 
-        printf("WSAStartup failed with error %d\n", RetCode);
-
-        return 1;
-
+        fprintf(stderr, "WSAStartup() failed");
+        exit(EXIT_FAILURE);
+        
     }
 
-    else
+    
 
-    {
-
-        printf("The Winsock dll found!\n");
-
-        printf("The current status is: %s.\n", wsaData.szSystemStatus);
-
-    }
-
-
-
-    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
-
-    {
-
-        //Tell the user that we could not find a usable WinSock DLL
-
-        printf("The dll do not support the Winsock version %u.%u!\n",
-
-            LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
-
-        // When your application is finished call WSACleanup
-
-        WSACleanup();
-
-        // and exit
-
-        return 0;
-
-    }
-
-    else
-
-    {
-
-        printf("The dll supports the Winsock version %u.%u!\n", LOBYTE(wsaData.wVersion),
-
-            HIBYTE(wsaData.wVersion));
-
-        printf("The highest version this dll can support: %u.%u\n", LOBYTE(wsaData.wHighVersion),
-
-            HIBYTE(wsaData.wHighVersion));
-
-
-
-        // Setup Winsock communication code here
-
-
-
-        // When your application is finished call WSACleanup
-
-        if (WSACleanup() == SOCKET_ERROR)
-
-            printf("WSACleanup failed with error %d\n", WSAGetLastError());
-
-        // and exit
-
-        return 1;
-
-    }
+    
 
 }
